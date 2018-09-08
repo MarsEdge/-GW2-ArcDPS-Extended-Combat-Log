ea895853f35f28dbab21018b9885a9d01e8eb4c4
Marty Rand <marsedge95@gmail.com>
2018-09-08 15:35:09 -0500

    Move arc structs to arcdps_datastructures.h

--- arcdps_combatdemo.cpp
+++ arcdps_combatdemo.cpp
@@ -7,66 +7,9 @@
 #include <Windows.h>
 #include <string>
 #include <mutex>
+#include "arcdps_datastructures.h"
 #include "imgui.h"
 #include "imgui_panels.h"
-
-/* arcdps export table */
-typedef struct arcdps_exports {
-	uintptr_t size; /* arcdps internal use, ignore */
-	uintptr_t sig; /* pick a non-zero number that isn't used by other modules */
-	char* out_name; /* name string */
-	char* out_build; /* build string */
-	void* wnd_nofilter; /* wndproc callback, fn(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam) */
-	void* combat; /* combat event callback, fn(cbtevent* ev, ag* src, ag* dst, char* skillname) */
-	void* imgui; /* id3dd9::present callback, before imgui::render, fn() */
-	void* options; /* id3dd9::present callback, appending to the end of options window in arcdps, fn() */
-	void* combat_local;  /* combat event callback like area but from chat log, fn(cbtevent* ev, ag* src, ag* dst, char* skillname, uint64_t id, uint64_t revision) */
-} arcdps_exports;
-
-/* combat event */
-typedef struct cbtevent {
-	uint64_t time; /* timegettime() at time of event */
-	uint64_t src_agent; /* unique identifier */
-	uint64_t dst_agent; /* unique identifier */
-	int32_t value; /* event-specific */
-	int32_t buff_dmg; /* estimated buff damage. zero on application event */
-	uint16_t overstack_value; /* estimated overwritten stack duration for buff application */
-	uint16_t skillid; /* skill id */
-	uint16_t src_instid; /* agent map instance id */
-	uint16_t dst_instid; /* agent map instance id */
-	uint16_t src_master_instid; /* master source agent map instance id if source is a minion/pet */
-	uint8_t iss_offset; /* internal tracking. garbage */
-	uint8_t iss_offset_target; /* internal tracking. garbage */
-	uint8_t iss_bd_offset; /* internal tracking. garbage */
-	uint8_t iss_bd_offset_target; /* internal tracking. garbage */
-	uint8_t iss_alt_offset; /* internal tracking. garbage */
-	uint8_t iss_alt_offset_target; /* internal tracking. garbage */
-	uint8_t skar; /* internal tracking. garbage */
-	uint8_t skar_alt; /* internal tracking. garbage */
-	uint8_t skar_use_alt; /* internal tracking. garbage */
-	uint8_t iff; /* from iff enum */
-	uint8_t buff; /* buff application, removal, or damage event */
-	uint8_t result; /* from cbtresult enum */
-	uint8_t is_activation; /* from cbtactivation enum */
-	uint8_t is_buffremove; /* buff removed. src=relevant, dst=caused it (for strips/cleanses). from cbtr enum  */
-	uint8_t is_ninety; /* source agent health was over 90% */
-	uint8_t is_fifty; /* target agent health was under 50% */
-	uint8_t is_moving; /* source agent was moving */
-	uint8_t is_statechange; /* from cbtstatechange enum */
-	uint8_t is_flanking; /* target agent was not facing source */
-	uint8_t is_shields; /* all or partial damage was vs barrier/shield */
-	uint8_t is_offcycle; /* zero if buff dmg happened during tick, non-zero otherwise */
-	uint8_t pad64; /* internal tracking. garbage */
-} cbtevent;
-
-/* agent short */
-typedef struct ag {
-	char* name; /* agent name. may be null. valid only at time of event. utf8 */
-	uintptr_t id; /* agent unique identifier */
-	uint32_t prof; /* profession at time of event. refer to evtc notes for identification */
-	uint32_t elite; /* elite spec at time of event. refer to evtc notes for identification */
-	uint32_t self; /* 1 if self, 0 if not */
-} ag;
 
 /* proto/globals */
 uint32_t cbtcount = 0;
@@ -79,7 +22,7 @@ extern "C" __declspec(dllexport) void* get_release_addr();
 arcdps_exports* mod_init();
 uintptr_t mod_release();
 uintptr_t mod_wnd(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
-uintptr_t mod_combat(cbtevent* ev, ag* src, ag* dst, char* skillname);
+uintptr_t mod_combat(cbtevent* ev, ag* src, ag* dst, char* skillname, uint64_t id, uint64_t revision);
 uintptr_t mod_imgui();
 uintptr_t mod_options();
 
@@ -179,7 +122,7 @@ uintptr_t mod_wnd(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
 
 /* combat callback -- may be called asynchronously. return ignored */
 /* one participant will be party/squad, or minion of. no spawn statechange events. despawn statechange only on marked boss npcs */
-uintptr_t mod_combat(cbtevent* ev, ag* src, ag* dst, char* skillname) {
+uintptr_t mod_combat(cbtevent* ev, ag* src, ag* dst, char* skillname, uint64_t id, uint64_t revision) {
 
 	if(!show_log && !show_console) return 0;
 
@@ -190,29 +133,30 @@ uintptr_t mod_combat(cbtevent* ev, ag* src, ag* dst, char* skillname) {
 
 	/* ev is null. dst will only be valid on tracking add. skillname will also be null */
 	if (!ev) {
+		if (src) {
+			/* notify tracking change */
+			if (!src->elite) {
+				if (!show_tracking_change) return 0;
+				/* add */
+				if (src->prof) {
+					p += _snprintf(p, 400, "==== cbtnotify ====\n");
+					// self flag disabled - always 1
+					p += _snprintf(p, 400, "agent added: %s:%s (%0llx), prof: %u, elite: %u, self: %u\n", src->name, dst->name, src->id, dst->prof, dst->elite, dst->self);
+				}
 
-		/* notify tracking change */
-		if (!src->elite) {
-            if(!show_tracking_change) return 0;
-			/* add */
-			if (src->prof) {
-				p += _snprintf(p, 400, "==== cbtnotify ====\n");
-				// self flag disabled - always 1
-				p += _snprintf(p, 400, "agent added: %s:%s (%0llx), prof: %u, elite: %u, self: %u\n", src->name, dst->name, src->id, dst->prof, dst->elite, dst->self);
+				/* remove */
+				else {
+					p += _snprintf(p, 400, "==== cbtnotify ====\n");
+					p += _snprintf(p, 400, "agent removed: %s (%llx)\n", src->name, src->id);
+				}
 			}
 
-			/* remove */
-			else {
+			/* notify target change */
+			else if (src->elite == 1) {
+				if (!show_target_change) return 0;
 				p += _snprintf(p, 400, "==== cbtnotify ====\n");
-				p += _snprintf(p, 400, "agent removed: %s (%llx)\n", src->name, src->id);
+				p += _snprintf(p, 400, "new target: %llx\n", src->id);
 			}
-		}
-
-		/* notify target change */
-		else if (src->elite == 1) {
-            if(!show_target_change) return 0;
-			p += _snprintf(p, 400, "==== cbtnotify ====\n");
-			p += _snprintf(p, 400, "new target: %llx\n", src->id);
 		}
 	}
 
